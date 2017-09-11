320512c3f975366c92417b74a7e970bd921159d9
Matyro <dominik.baack@udo.edu>
2017-09-12 00:33:29 +0200

    Work on RC tests with gtest

--- src/test/remote_control/unit_test.cpp
+++ src/test/remote_control/unit_test.cpp
@@ -16,64 +16,10 @@ namespace test
 {
 	namespace remote_control
 	{
-		bool test_mesageQueue()
-		{
-			bool erg = true;
-
-			std::cout << "\n\t-Single Insert-" << std::endl;
-			erg = erg && test::remote_control::test_mq_singleInsert();
-
-			std::cout << "\n\t-Threaded Insert-" << std::endl;
-			erg = erg && test::remote_control::test_mq_threadedInsert();
-
-			return erg;
-		}
-
-		bool test_protocol()
-		{
-			bool erg = true;
-
-			std::cout << "\n\t-Protocol construction-" << std::endl;
-			erg = erg && test::remote_control::test_protocol_construction();
-
-			return erg;
-		}
-
-	}
-
-	bool test_remoteControl()
-	{
-		std::cout << std::string(8, '\n');
-		std::cout << "\t\t\t--- RemoteControl Test ---" << std::endl;
-
-		bool erg = true;	
-
-		std::cout << "\n\n\t\t--Test protocol--" << std::endl;
-		erg = erg && test::remote_control::test_protocol();
-
-		std::cout << "\n\n\t\t--Test message queue--" << std::endl;
-		erg = erg && test::remote_control::test_mesageQueue();
-
-		return erg;
-	}
+        int main(int argc, char **argv)
+        {
+            ::testing::InitGoogleTest(&argc, argv);
+            return RUN_ALL_TESTS();
+        }
+    }
 }
-
-
-
-int main()
-{
-        std::cout << "Start testing ... \n" << std::endl;
-
-        test::test_remoteControl();
-
-
-
-
-
-        std::cout << "\n\n\t All tests finished" << std::endl;
-
-        return 0;
-
-}
-
-
