cf8645bed9107425c842e07e38d2517aaab70398
Dominik Baack <dominik.baack@udo.edu>
2017-09-15 02:17:45 +0200

    Added first instance of main function

---
+++ src/CorPP.cpp
@@ -0,0 +1,26 @@
+/* CorPP.cpp
+* this file is part of CorPP for CORSIKA
+*
+* Copyright (C) <2017> <Dominik Baack>
+*		All rights reserved.
+*
+* 	This software may be modified and distributed under the terms
+* 	of the LGPL license. See the LICENSE file for details.
+*/
+
+
+#include "import_functions.h"
+
+
+int main(int argc, char** argv)
+{
+    AAMIN();
+
+
+
+
+
+
+
+    return 0;
+}
